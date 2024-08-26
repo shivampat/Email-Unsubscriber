119039a4d4aa12f1140008a5623b09199b72b169
Shivam Patel <shivampatel3301@gmail.com>
2024-08-25 20:27:25 -0400

    added fetch functionality for header API

---
+++ Python backend/Web server/static/styles/script.js
@@ -0,0 +1,25 @@
+console.log("Getting links...");
+
+fetch("https://localhost:5000/get_links")
+  .then((response) => response.json())
+  .then((data) => {
+    const header_div = document.getElementById("headers");
+    Object.entries(data).forEach(([key, value]) => {
+      const headers = value.header;
+      headers.forEach((link) => {
+        const p_elm = document.createElement("a");
+        const horizontal_line = document.createElement("hr");
+        link = link.replace("<", "").replace(">", "");
+
+        if (link.indexOf("https") != -1)
+        else if (link.indexOf("mailto:"))
+          link = link.replace("<mailto:", "").replace(">", "");
+
+        p_elm.href = link
+        p_elm.innerText = link
+
+        header_div.append(p_elm);
+        header_div.append(horizontal_line)
+      });
+    });
+  });
